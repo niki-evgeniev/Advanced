dbe9418fbcf55d38f2a3c6ef95dd2c498d3fbb9c
Nikolay Ivanov <niki.evgeniev@gmail.com>
2023-01-13 01:53:40 +0200

    Create MergeTwoFiles07.java

---
+++ StreamsFilesandDirectoriesExercise/MergeTwoFiles07.java
@@ -0,0 +1,22 @@
+package StreamsFilesandDirectoriesExercise;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+
+public class MergeTwoFiles07 {
+    public static void main(String[] args) throws IOException {
+        Path firstFile = Paths.get("src/StreamsFilesandDirectoriesExercise/inputOne.txt");
+        List<String> firstFileLines = Files.readAllLines(firstFile);
+
+        Path secondFile = Paths.get("src/StreamsFilesandDirectoriesExercise/inputTwo.txt");
+        List<String> secondFileLines = Files.readAllLines(secondFile);
+
+        Path output = Paths.get("src/StreamsFilesandDirectoriesExercise/output.txt");
+        Files.write(output, firstFileLines, StandardOpenOption.APPEND);
+        Files.write(output, secondFileLines, StandardOpenOption.APPEND);
+    }
+}
