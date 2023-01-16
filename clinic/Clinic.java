aaa0e78dc8daaf09650eee624197c7cfe3820fd6
Nikolay Ivanov <niki.evgeniev@gmail.com>
2023-01-16 02:02:38 +0200

    Create Clinic.java

---
+++ clinic/Clinic.java
@@ -0,0 +1,62 @@
+package clinic;
+
+import vetClinic.Pet;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Clinic {
+    private int capacity;
+    private List<vetClinic.Pet> data;
+
+    public Clinic(int capacity) {
+
+        this.data = new ArrayList<>();
+        this.capacity = capacity;
+    }
+
+
+    public void add(vetClinic.Pet pet) {
+        if (data.size() != capacity) {
+            data.add(pet);
+        }
+    }
+
+    public boolean remove(String name) {
+        for (vetClinic.Pet pet : data) {
+            String names = pet.getName();
+            if (names.equals(name)){
+                data.remove(pet);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public vetClinic.Pet getPet(String name, String owner){
+        for (vetClinic.Pet pet : data) {
+            if (pet.getName().equals(name) && pet.getOwner().equals(owner)){
+                return pet;
+            }
+        }
+        return null;
+    }
+    public vetClinic.Pet getOldestPet(){
+        return Collections.max(data, (f, s) -> Integer.compare(f.getAge(), s.getAge()));
+
+    }
+
+    public int getCount() {
+        return data.size();
+    }
+    public String getStatistics() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("The clinic has the following patients:");
+        for (Pet pet : data) {
+            sb.append(System.lineSeparator());
+            sb.append(pet.getName()).append(" ").append(pet.getOwner());
+        }
+        return sb.toString();
+    }
+}
