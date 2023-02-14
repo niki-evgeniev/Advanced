9ff1f806864845c59d3ef9637a454fc165585b32
Nikolay Ivanov <niki.evgeniev@gmail.com>
2023-02-15 00:39:51 +0200

    Create Trainer.java

---
+++ ExercisesDefiningClasses/Trainer.java
@@ -0,0 +1,9 @@
+package ExercisesDefiningClasses;
+
+public class Trainer {
+    public String nameTrainer;
+
+    public Trainer (String nameTrainer){
+        this.nameTrainer = nameTrainer;
+    }
+}
