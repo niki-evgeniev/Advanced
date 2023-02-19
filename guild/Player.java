8024dc5fd720eb28eabe187dd45e677523e67d72
Nikolay Ivanov <niki.evgeniev@gmail.com>
2023-02-20 00:55:45 +0200

    Create Player.java

---
+++ guild/Player.java
@@ -0,0 +1,52 @@
+package guild;
+
+public class Player {
+    private String name;
+    private String clazz;
+    private String rank = "Trial";
+    private String description = "n/a";
+
+    public Player(String name, String clazz) {
+        this.name = name;
+        this.clazz = clazz;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getClazz() {
+        return clazz;
+    }
+
+    public void setClazz(String clazz) {
+        this.clazz = clazz;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Player %s: %s%n" +
+                "Rank: %s%n" +
+                "Description: %s", name, clazz, rank, description);
+    }
+}
