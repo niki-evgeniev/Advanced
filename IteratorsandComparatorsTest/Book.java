1716a60bdcf22283e259ed364f4d817c21ee38e7
Nikolay Ivanov <niki.evgeniev@gmail.com>
2023-02-21 23:56:58 +0200

    Create Book.java

---
+++ IteratorsandComparatorsTest/Book.java
@@ -0,0 +1,44 @@
+package IteratorsandComparatorsTest;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Book {
+    private String title;
+    private int year;
+    private List<String> authors;
+
+    public Book(String title, int year, String... authors) {
+        setTitle(title);
+        setYear(year);
+        setAuthors(authors);
+    }
+
+    private void setAuthors(String[] authors) {
+        this.authors = Arrays.asList(authors);
+    }
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+
+
+    private void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+
+    private void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+
+}
